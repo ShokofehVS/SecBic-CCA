a51c5e31a575abe0cd112f0afba222baff13fcb0
ShokofehVS <sh.vahidian@gmail.com>
2022-12-08 15:22:07 +0100

    Update description, imports

--- SecBiclib/evaluation/__init__.py
+++ SecBiclib/evaluation/__init__.py
@@ -1,15 +1,12 @@
 """
-    SeCCA: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
+    SecBic-CCA: A Python library of privacy-preserving biclustering algorithm (Cheng and Church) with Homomorphic Encryption
+
     Copyright (C) 2022  Shokofeh VahidianSadegh
-    This file is part of SeCCA.
+
+    This file is part of SecBic-CCA.
+
 """
 
 from .subspace import clustering_error
 from .subspace import relative_non_intersecting_area
-
-from .prelic import prelic_relevance
-from .prelic import prelic_recovery
-
-from .liu_wang import liu_wang_match_score
-
 from .csi import csi
