39c4c3b3f94d20c65d283af49a85cd8eec9d682a
ShokofehVS <sh.vahidian@gmail.com>
2022-12-08 14:48:10 +0100

    Update description

--- biclustlib/algorithms/cca.py
+++ biclustlib/algorithms/cca.py
@@ -1,9 +1,9 @@
 """
-    SecBic-SeCCA: A Python library of privacy-preserving biclustering algorithm (Cheng and Church) with Homomorphic Encryption
+    SecBic-CCA: A Python library of privacy-preserving biclustering algorithm (Cheng and Church) with Homomorphic Encryption
 
     Copyright (C) 2022  Shokofeh VahidianSadegh
 
-    This file is part of SecBic-SeCCA.
+    This file is part of SecBic-CCA.
 
 """
 
