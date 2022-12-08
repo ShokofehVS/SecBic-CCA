d2b4889db44374e3577760f5d668afa4e3ffd3bb
ShokofehVS <sh.vahidian@gmail.com>
2022-12-08 15:39:24 +0100

    Remove unnecessary imports, methods

--- SecBiclib/scripts/evaluation.py
+++ SecBiclib/scripts/evaluation.py
@@ -1,41 +1,24 @@
 from SecBiclib.algorithms import ChengChurchAlgorithm
 from SecBiclib.algorithms import SecuredChengChurchAlgorithm
-from SecBiclib.algorithms import SecuredChengChurchAlgorithmType1
-from SecBiclib.algorithms import SecuredChengChurchAlgorithmType2
-from SecBiclib.algorithms import SecuredChengChurchAlgorithmType3
-from SecBiclib.algorithms import SecuredChengChurchAlgorithmType4
 from SecBiclib.evaluation import clustering_error
 from SecBiclib.datasets import load_yeast_tavazoie
-import matplotlib.pyplot as plt
 
-
+# load yeast data used in the original Cheng and Church's paper
 data = load_yeast_tavazoie().values
 num_rows, num_cols = data.shape
 
+
+# creating an instance of the ChengChurchAlgorithm, SecuredChengChurchAlgorithm classes
+# and running with the parameters of the original study
 cca = ChengChurchAlgorithm(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
 secca = SecuredChengChurchAlgorithm(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
-type1 = SecuredChengChurchAlgorithmType1(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
-type2 = SecuredChengChurchAlgorithmType2(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
-type3 = SecuredChengChurchAlgorithmType3(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
-type4 = SecuredChengChurchAlgorithmType4(num_biclusters=5, msr_threshold=300.0, multiple_node_deletion_threshold=1.2)
-
-# secca_alog = [type1, type2, type3, type4, secca]
-secca_alog = [type3]
 biclustering_ref = cca.run(data)
+biclustering_pre = secca.run(data)
 
-ce_eval = []
-
-for i in range(len(secca_alog)):
-    biclustering_pre = secca_alog[i].run(data)
-    ce_eval.append(round(clustering_error(biclustering_pre, biclustering_ref, num_rows, num_cols),5))
+# creating an instance of the clustering error class
+# and running with reference and predicted algorithms
+ce_eval = round(clustering_error(biclustering_pre, biclustering_ref, num_rows, num_cols),5)
 
 print(ce_eval)
-# secured_alg = ["Type1", "Type2", "Type3", "Type4", "Total"]
-#
-# plt.bar(secured_alg, ce_eval, color='blue')
-# plt.title('Comparison of SeCCA with CCA')
-# plt.xlabel('Types of SeCCA')
-# plt.ylabel('CE External Evaluation Measure ')
-# plt.savefig('CE_final.png')
-# plt.show()
+
 
