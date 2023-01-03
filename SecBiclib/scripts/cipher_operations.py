eb5246ab996f32d5e05419388a634468a28e265c
Julian <rausch@iap.uni-frankfurt.de>
2023-01-03 11:50:46 +0100

    row-wise sum working

--- SecBiclib/scripts/cipher_operations.py
+++ SecBiclib/scripts/cipher_operations.py
@@ -5,12 +5,20 @@ def reshape(array, shape):
     sub_array=array[:(shape[0]*shape[1])]
     return sub_array.reshape(shape)
 def shift(array,by):
-    a_shape=array.shape
-    flat_shifted=array.flatten()
-    for i in range(by):
-        flat_shifted=np.array([flat_shifted[i] for i in range(-1,len(flat_shifted)-1)])
-    shifted=flat_shifted.reshape(a_shape)
-    return shifted
+    if not isinstance(by,int):
+        raise TypeError('Shift distance has to be integer but was given as: '+str(type(by)))
+    elif (abs(by) >= array.size):
+        raise ValueError('Shift distance has to be smaller than size of array')
+    if by==0:
+        return array
+    else:
+        a_shape=array.shape
+        flat=array.flatten()
+        flat_shifted=flat.copy()
+        flat_shifted[:by]=flat[-by:]
+        flat_shifted[by:]=flat[:-by]
+        shifted=flat_shifted.reshape(a_shape)
+        return shifted
 
 HE = Pyfhel()
 ckks_params = {
@@ -33,38 +41,49 @@ print("Data-Array:")
 print(data)
 
 #Make bigger array with all rows, cols needed for shifting:
-sup_array=np.array([[data[i,j] for j in range(-N_cols,N_cols)] for i in range(-N_rows,N_rows)])
+sup_data=np.array([[data[i,j] for j in range(-N_cols,N_cols)] for i in range(-N_rows,N_rows)])
 real_N_rows=2*N_rows
 real_N_cols=2*N_cols
 #print("With added rows, cols:")
 #print(sup_array)
 #print("Converted to 1d:")
-flat_sup_array=sup_array.flatten()
+flat_sup_data=sup_data.flatten()
 #print(flat_sup_array)
-
-c_data = HE.encryptFrac(flat_sup_array) # Encrypts the plaintext ptxt_x and returns a PyCtxt
+c_data = HE.encryptFrac(flat_sup_data) # Encrypts the plaintext ptxt_x and returns a PyCtxt
 
 c_col_sum=c_data.copy()
-col_sum=data.copy()
-
+c_row_sum=c_data.copy()
+col_sum=sup_data.copy()
+row_sum=sup_data.copy()
 print("Shift and sum:")
 print(c_data)
 
-
-c_sum=c_data.copy()
-test_shift=c_data << 1*real_N_cols
-test_shift=c_data << 2*real_N_cols
-c_sum+=test_shift
+# testing shifts:
+#c_sum=c_data.copy()
+#test_shift=c_data << 1*real_N_cols
+#test_shift=c_data << 2*real_N_cols
+#c_sum+=test_shift
 
 # col-wise sum:
 for i in range(1,N_rows):
     c_col_sum+=c_data << real_N_cols*i
-    col_sum+=shift(data,N_cols*i)
-    print(c_col_sum)
+    col_sum+=shift(sup_data,real_N_cols*i)
 print("COLUMN-WISE SUM:")
 #result=reshape(HE.decryptFrac(c_sum),(real_N_rows,real_N_cols))
 result=reshape(HE.decryptFrac(c_col_sum),(real_N_rows,real_N_cols))
 print("SHOULD BE:\n",col_sum,"\nRESULT:\n",result)
+
+# row-wise sum:
+print("ROW-WISE SUM:")
+for i in range(1,N_cols):
+    c_row_sum+=c_data << i
+    shifted=shift(sup_data,-i)
+    row_sum+=shifted
+#result=reshape(HE.decryptFrac(c_sum),(real_N_rows,real_N_cols))
+result=reshape(HE.decryptFrac(c_row_sum),(real_N_rows,real_N_cols))
+print("SHOULD BE:\n",row_sum,"\nRESULT:\n",result)
+print("ERRORS:\n",result-row_sum)
+
 #print([result[i] for i in range(len(result))])
 
 ##  1. Mean
