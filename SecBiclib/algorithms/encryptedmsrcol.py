4a0ff6a96e3f326293eadeb442bd694f1cf783ad
ShokofehVS <sh.vahidian@gmail.com>
2023-02-09 16:28:08 +0100

    Correct method inside list msr_col calc

--- SecBiclib/algorithms/encryptedmsrcol.py
+++ SecBiclib/algorithms/encryptedmsrcol.py
@@ -191,7 +191,7 @@ class ClacEncMSRCol:
                 cipher_col_residue.append(cipher_data_rows[i] - cipher_row_mean[i] - cipher_col_mean[i] + cipher_data_mean[i])
                 cipher_col_square_residue.append(cipher_col_residue[i] ** 2)
                 HE.rescale_to_next(cipher_col_square_residue[i])
-                cipher_col_msr.append(self.data_mean(HE, ~cipher_col_square_residue[i], data_size))
+                cipher_col_msr.append(self.col_mean(HE, ~cipher_col_square_residue[i], data_size))
 
         else:
             cipher_col_residue = cipher_data_rows - cipher_row_mean - cipher_col_mean + cipher_data_mean
